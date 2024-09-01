d8691b09f9dfafcc13aee8290ca22648331fb6ce
abramsymons <abramsymons@gmail.com>
2024-09-01 09:41:17 +0330

    use mpc wallet

--- wallet/Wallet.sol
+++ wallet/Wallet.sol
@@ -3,25 +3,45 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {BLSSignatureChecker, IRegistryCoordinator} from "@eigenlayer-middleware/src/BLSSignatureChecker.sol";
 
-contract Wallet is Ownable, BLSSignatureChecker {
+interface ISchnorrVerifier {
+    function verifySignature(
+        uint256 pubKeyX,
+        uint8 pubKeyYParity,
+        uint256 signature,
+        uint256 msgHash,
+        address nonceTimesGeneratorAddress
+    ) external view returns (bool);
+}
 
-    uint256 internal constant _THRESHOLD_DENOMINATOR = 100;
-    uint256 internal constant _THRESHOLD_PERCENTAGE = 70;
-
-    mapping(address => uint256) public nonces;
-    mapping(address => uint256) private tokenToIndex;
-    mapping(uint256 => address) private indexToToken;
-    uint256 private nextTokenIndex = 1;
+contract Wallet is Ownable {
+    uint256 public pubKeyX;
+    uint8 public pubKeyYParity;
+    ISchnorrVerifier public verifier;
+    mapping(uint256 => uint256) public nonces;
+    mapping(address => uint256) public tokenToIndex;
+    mapping(uint256 => address) public indexToToken;
+    uint256 public nextTokenIndex = 1;
 
     event Deposit(uint256 indexed tokenIndex, uint256 amount, uint256 indexed pubKeyX, uint8 indexed pubKeyYParity);
     event Withdrawal(address indexed token, address indexed to, uint256 amount);
     event PublicKeySet(uint256 indexed pubKeyX, uint8 indexed pubKeyYParity);
 
-    constructor(
-        IRegistryCoordinator _registryCoordinator
-    ) BLSSignatureChecker(_registryCoordinator) {}
+    constructor(address _verifier, uint256 _pubKeyX, uint8 _pubKeyYParity) Ownable(msg.sender) {
+        verifier = ISchnorrVerifier(_verifier);
+        pubKeyX = _pubKeyX;
+        pubKeyYParity = _pubKeyYParity;
+    }
+
+    function setVerifier(address _verifier) external onlyOwner {
+        verifier = ISchnorrVerifier(_verifier);
+    }
+
+    function setPublicKey(uint256 _pubKeyX, uint8 _pubKeyYParity) external onlyOwner {
+        pubKeyX = _pubKeyX;
+        pubKeyYParity = _pubKeyYParity;
+        emit PublicKeySet(pubKeyX, pubKeyYParity);
+    }
 
     function deposit(address token, uint256 amount, uint256 _publicKeyX, uint8 _pubKeyYParity) external {
         require(amount > 0, "Amount must be greater than 0");
@@ -41,37 +61,21 @@ contract Wallet is Ownable, BLSSignatureChecker {
     function withdraw(
         uint256 tokenIndex,
         uint256 amount,
+        uint256 user,
         address dest,
         uint256 nonce,
-        bytes calldata quorumNumbers,
-        NonSignerStakesAndSignature memory nonSignerStakesAndSignature
+        uint256 signature,
+        address nonceTimesGeneratorAddress
     ) external {
-        require(nonce == nonces[dest], "Invalid nonce");
-        bytes32 message = keccak256(abi.encode(dest, tokenIndex, amount, nonce));
+        require(nonce == nonces[user], "Invalid nonce");
+        uint256 msgHash = uint256(keccak256(abi.encodePacked(user, dest, tokenIndex, amount, nonce)));
 
-        // check the BLS signature
-        (
-            QuorumStakeTotals memory quorumStakeTotals,
-            bytes32 hashOfNonSigners
-        ) = checkSignatures(
-                message,
-                quorumNumbers,
-                uint32(block.number) - 1,
-                nonSignerStakesAndSignature
-            );
+        require(
+            verifier.verifySignature(pubKeyX, pubKeyYParity, signature, msgHash, nonceTimesGeneratorAddress),
+            "Invalid signature"
+        );
 
-        // check that signatories own at least a threshold percentage of each quourm
-        for (uint i = 0; i < quorumNumbers.length; i++) {
-            require(
-                quorumStakeTotals.signedStakeForQuorum[i] *
-                    _THRESHOLD_DENOMINATOR >=
-                    quorumStakeTotals.totalStakeForQuorum[i] *
-                        _THRESHOLD_PERCENTAGE,
-                "Signatories do not own at least threshold percentage of a quorum"
-            );
-        }
-
-        nonces[dest] += 1;
+        nonces[user] += 1;
 
         address token = indexToToken[tokenIndex];
         bool sent = IERC20(token).transfer(dest, amount);
