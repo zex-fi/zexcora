e22ec8f41c572f6279881928880ce9ad532466c0
Mohammad Shoaei <mshoaei@proton.me>
2024-09-03 20:31:35 +0330

    updated contract

--- wallet/Wallet.sol
+++ wallet/Wallet.sol
@@ -19,6 +19,7 @@ contract Wallet is Ownable {
     uint8 public pubKeyYParity;
     ISchnorrVerifier public verifier;
     mapping(uint256 => uint256) public nonces;
+    uint256[] public users;
     mapping(address => uint256) public tokenToIndex;
     mapping(uint256 => address) public indexToToken;
     uint256 public nextTokenIndex = 1;
@@ -26,6 +27,7 @@ contract Wallet is Ownable {
     event Deposit(uint256 indexed tokenIndex, uint256 amount, uint256 indexed pubKeyX, uint8 indexed pubKeyYParity);
     event Withdrawal(address indexed token, address indexed to, uint256 amount);
     event PublicKeySet(uint256 indexed pubKeyX, uint8 indexed pubKeyYParity);
+    event WithdrawNonceReset();
 
     constructor(address _verifier, uint256 _pubKeyX, uint8 _pubKeyYParity) Ownable(msg.sender) {
         verifier = ISchnorrVerifier(_verifier);
@@ -58,6 +60,29 @@ contract Wallet is Ownable {
         emit Deposit(tokenIndex, amount, _publicKeyX, _pubKeyYParity);
     }
 
+    // Function to add or update a nonce for a user
+    function setNonce(uint256 userId, uint256 nonce) public {
+        if (nonces[userId] == 0 && nonce != 0) {
+            users.push(userId);
+        }
+        nonces[userId] = nonce;
+    }
+
+    // Function to get the current nonce for a user
+    function getNonce(uint256 userId) public view returns (uint256) {
+        return nonces[userId];
+    }
+
+    //////////////// Warning /////////////////
+    // Function to reset all nonces to 0 /////
+    //////// This is only for test ///////////
+    function resetAllNonces() public {
+        for (uint i = 0; i < users.length; i++) {
+            nonces[users[i]] = 0;
+        }
+        delete users;
+    }
+
     function withdraw(
         uint256 tokenIndex,
         uint256 amount,
@@ -67,7 +92,7 @@ contract Wallet is Ownable {
         uint256 signature,
         address nonceTimesGeneratorAddress
     ) external {
-        require(nonce == nonces[user], "Invalid nonce");
+        require(nonce == getNonce(user), "Invalid nonce");
         uint256 msgHash = uint256(keccak256(abi.encodePacked(user, dest, tokenIndex, amount, nonce)));
 
         require(
@@ -75,7 +100,7 @@ contract Wallet is Ownable {
             "Invalid signature"
         );
 
-        nonces[user] += 1;
+        setNonce(user, nonce + 1);
 
         address token = indexToToken[tokenIndex];
         bool sent = IERC20(token).transfer(dest, amount);
