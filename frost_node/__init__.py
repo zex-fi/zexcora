23554ec77a8ae6ec0f1ef4eba46a62ab7435aaf4
Mohammad Shoaei <mshoaei@proton.me>
2024-09-27 22:03:04 +0330

    added frost node implementation

---
+++ frost_node/__init__.py
@@ -0,0 +1,9 @@
+from .abstracts import NodeDataManager, NodesInfo, NodeValidators
+from .config import VALIDATED_IPS
+
+__all__ = [
+    NodeDataManager,
+    NodesInfo,
+    NodeValidators,
+    VALIDATED_IPS,
+]
---
+++ frost_node/config.py
@@ -0,0 +1,9 @@
+VALIDATED_IPS = {
+    "127.0.0.1": [
+        "/pyfrost/v1/dkg/round1",
+        "/pyfrost/v1/dkg/round2",
+        "/pyfrost/v1/dkg/round3",
+        "/pyfrost/v1/sign",
+        "/pyfrost/v1/generate-nonces",
+    ]
+}
