a83a17dfd8ca769074cc6f7a5bc260c3223ec5f7
Mohammad Shoaei <mshoaei@proton.me>
2025-01-17 00:36:10 +0330

    chore: migrate to testnet
    (cherry picked from commit 9e79116fe4c40f879aacbb1b60b33010e7a632ed)

--- bot/markets.py
+++ bot/markets.py
@@ -1,28 +1,14 @@
-QUOTES = {"BTC": [1], "POL": [1, 2]}
-TOKENS = {
-    "BTC": [1],
-    "POL": [1, 2, 3, 4],
-    "BSC": [1, 2, 3, 4],
-    "ARB": [1, 2, 3, 4],
-    "OPT": [1, 2, 3, 4],
-}
 PAIRS = [
     {
-        "pair": "BTC-USDT",
+        "pair": "zEIGEN-zUSDT",
         "price_digits": 2,
         "volume_digits": 5,
-        "binance_name": "BTCUSDT",
+        "binance_name": "EIGENUSDT",
     },
     {
-        "pair": "LINK-USDT",
+        "pair": "zWBTC-zUSDT",
         "price_digits": 2,
         "volume_digits": 2,
-        "binance_name": "LINKUSDT",
-    },
-    {
-        "pair": "POL:0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619-USDT",
-        "price_digits": 2,
-        "volume_digits": 5,
-        "binance_name": "WBETHUSDT",
+        "binance_name": "WBTCUSDT",
     },
 ]
